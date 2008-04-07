b2597ddcd4e749aae2a0b1e92e619b865835427a
bowmanjd <bowmanjd@b2bf4607-bf48-0410-b63c-074d3d4365f4>
2008-04-07 21:28:17 +0000

    refactored

--- trunk/simple_modal.js
+++ trunk/simple_modal.js
@@ -1,89 +1,91 @@
-// SimpleModal JavaScript Library v0.7
+// Simple.Modal JavaScript Library v0.7
 // http://simple-javascripts.googlecode.com
 // Copyright (c) 2008 Jonathan Bowman and Eastern Mennonite University
 // MIT-style License <http://opensource.org/licenses/mit-license.php>
 
-var SimpleModal = {
-    accommodations: function (on) {
-        if (this.oldie) {
-            var visibility;
-            if (on) {
-                this.hidden_selects = document.getElementsByTagName('select');
-                visibility = 'hidden';
-                this.add_event(window, 'scroll', this.position);
-                this.add_event(window, 'resize', function () {
-                    SimpleModal.position();
-                    SimpleModal.stretch();
-                });
-                this.position();
-                this.stretch();
-            } else {
-                visibility = 'visible';
-                window.onscroll = this.oldscroll || null;
-                window.onresize = this.oldresize || null;
-            }
-            for (var i = 0; i < this.hidden_selects.length; i++) {
-                this.hidden_selects[i].style.visibility = visibility;
-            }
-        }
-    },
+var Simple = {
     add_event: function (obj, evt, func) {
         if (typeof obj['on' + evt] === 'function') {
             this['old' + evt] = obj['on' + evt];
-            obj['on' + evt] = function () {
-                this['old' + evt]();
-                func();
+            obj['on' + evt] = function (evnt) {
+                this['old' + evt](evnt);
+                func(evnt);
             };
         } else {
             obj['on' + evt] = func;
         }
     },
-    close: function () {
-        this.center.style.visibility = 'hidden';
-        this.overlay.style.display = 'none';
-        this.accommodations(false);
-    },
-    open: function (content) {
-        this.box.innerHTML = content;
-        this.overlay.style.display = 'block';
-        this.box.style.marginLeft = '-' + (this.box.offsetWidth / 2).toString() + 'px';
-        this.box.style.top = '-' + (this.box.offsetHeight / 2).toString() + 'px';
-        this.accommodations(true);
-        this.center.style.visibility = 'visible';
-    },
     oldie: /*@cc_on(@_jscript_version < 5.7) || @*/false,
-    position: function () {
-        SimpleModal.center.style.top = (document.documentElement.scrollTop + (document.documentElement.clientHeight / 2)) + 'px';
-    },
-    setup: function () {
-        this.overlay = document.getElementById('simple_modal_overlay');
-        if (!this.overlay) {
-            this.overlay = document.createElement('div');
-            this.overlay.id = 'simple_modal_overlay';
+    Modal: {
+        accommodations: function (on) {
+            if (Simple.oldie) {
+                var visibility;
+                if (on) {
+                    this.hidden_selects = document.getElementsByTagName('select');
+                    visibility = 'hidden';
+                    Simple.add_event(window, 'scroll', this.position);
+                    Simple.add_event(window, 'resize', function () {
+                        Simple.Modal.position();
+                        Simple.Modal.stretch();
+                    });
+                    this.position();
+                    this.stretch();
+                } else {
+                    visibility = 'visible';
+                    window.onscroll = this.oldscroll || null;
+                    window.onresize = this.oldresize || null;
+                }
+                for (var i = 0; i < this.hidden_selects.length; i++) {
+                    this.hidden_selects[i].style.visibility = visibility;
+                }
+            }
+        },
+        close: function () {
+            this.center.style.visibility = 'hidden';
             this.overlay.style.display = 'none';
-            document.body.appendChild(this.overlay);
+            this.accommodations(false);
+        },
+        open: function (content) {
+            this.box.innerHTML = content;
+            this.overlay.style.display = 'block';
+            this.box.style.marginLeft = '-' + (this.box.offsetWidth / 2).toString() + 'px';
+            this.box.style.top = '-' + (this.box.offsetHeight / 2).toString() + 'px';
+            this.accommodations(true);
+            this.center.style.visibility = 'visible';
+        },
+        position: function () {
+            Simple.Modal.center.style.top = (document.documentElement.scrollTop + (document.documentElement.clientHeight / 2)) + 'px';
+        },
+        setup: function () {
+            this.overlay = document.getElementById('simple_modal_overlay');
+            if (!this.overlay) {
+                this.overlay = document.createElement('div');
+                this.overlay.id = 'simple_modal_overlay';
+                this.overlay.style.display = 'none';
+                document.body.appendChild(this.overlay);
+            }
+            this.center = document.getElementById('simple_modal_center');
+            if (!this.center) {
+                this.center = document.createElement('div');
+                this.center.id = 'simple_modal_center';
+                document.body.appendChild(this.center);
+            }
+            this.box = document.getElementById('simple_modal_box');
+            if (!this.box) {
+                this.box = document.createElement('div');
+                this.box.id = 'simple_modal_box';
+                this.center.appendChild(this.box);
+            }
+            if (Simple.oldie) {
+                this.overlay.style.position = 'absolute';
+                this.center.style.position = 'absolute';
+            }
+        },
+        stretch: function () {
+            Simple.Modal.overlay.style.height = (document.documentElement.clientHeight > document.body.offsetHeight ? document.documentElement.clientHeight : document.body.offsetHeight) + 'px';
         }
-        this.center = document.getElementById('simple_modal_center');
-        if (!this.center) {
-            this.center = document.createElement('div');
-            this.center.id = 'simple_modal_center';
-            document.body.appendChild(this.center);
-        }
-        this.box = document.getElementById('simple_modal_box');
-        if (!this.box) {
-            this.box = document.createElement('div');
-            this.box.id = 'simple_modal_box';
-            this.center.appendChild(this.box);
-        }
-        if (this.oldie) {
-            this.overlay.style.position = 'absolute';
-            this.center.style.position = 'absolute';
-        }
-    },
-    stretch: function () {
-        SimpleModal.overlay.style.height = (document.documentElement.clientHeight > document.body.offsetHeight ? document.documentElement.clientHeight : document.body.offsetHeight) + 'px';
     }
 };
-SimpleModal.add_event(window, 'load', function () {
-    SimpleModal.setup();
+Simple.add_event(window, 'load', function () {
+    Simple.Modal.setup();
 });
--- trunk/simple_tool_tip.js
+++ trunk/simple_tool_tip.js
@@ -1,4 +1,4 @@
-// ToolTip class
+// Tooltip class
 // Copyright (c) 2008 Jonathan Bowman and Eastern Mennonite University
 // MIT-style License <http://opensource.org/licenses/mit-license.php>
 //
@@ -7,125 +7,90 @@
 //        and make sure any child img tags with "alt" set also
 //        have "title" blank (so the alt text does not interfere.) 
 
-//ToolTip.className = 'widget'; // the class of element that triggers tooltips;
+//Simple.Tooltip.className = 'widget'; // the class of element that triggers tooltips;
                                 // leave blank to enable all titled elements
 
-function ToolTip() {
-    if (!document.getElementById('tool_tip_box')) {
-        this.box = document.createElement('div');
-        this.box.id = 'tool_tip_box';
-        this.box.style.position = 'absolute';
-        this.box.style.display = 'none';
-        document.body.appendChild(this.box);
-        var oldmouseover, oldmouseout;
-        if (typeof document.body.onmouseover === 'function') {
-            oldmouseover = document.body.onmouseover;
+var Simple = {
+    add_event: function (obj, evt, func) {
+        if (typeof obj['on' + evt] === 'function') {
+            this['old' + evt] = obj['on' + evt];
+            obj['on' + evt] = function (evnt) {
+                Simple['old' + evt](evnt);
+                func(evnt);
+            };
+        } else {
+            obj['on' + evt] = func;
         }
-        document.body.onmouseover = function (evt) {
-            if (oldmouseover) {
-                oldmouseover(evt);
+    },
+    oldie: /*@cc_on(@_jscript_version < 5.7) || @*/false,
+    Tooltip: {
+        hide: function () {
+            if (this.trigger) {
+                this.box.style.display = 'none';
+                this.trigger.title = this.title;
+                delete this.trigger;
             }
-            ToolTip.show(evt);
-        };
-        if (typeof document.body.onmouseout === 'function') {
-            oldmouseout = document.body.onmouseout;
-        }
-        document.body.onmouseout = function (evt) {
-            if (oldmouseout) {
-                oldmouseout(evt);
+        },
+        setup: function () {
+            if (!document.getElementById('tool_tip_box')) {
+                this.box = document.createElement('div');
+                this.box.id = 'tool_tip_box';
+                this.box.style.position = 'absolute';
+                this.box.style.display = 'none';
+                document.body.appendChild(this.box);
+                Simple.add_event(document.body, 'mouseover', function (evt) {
+                    Simple.Tooltip.show(evt);
+                });
+                Simple.add_event(document.body, 'mouseout', function () {
+                    Simple.Tooltip.hide();
+                });
             }
-            ToolTip.hide();
-        };
-    }
-}
-
-ToolTip.hide = function () {
-    if (ToolTip.instance) {
-        ToolTip.instance.hide();
-    }
-};
-ToolTip.init = function () {
-    var oldonload = window.onload;
-    if (typeof window.onload !== 'function') {
-        window.onload = ToolTip.load;
-    } else {
-        window.onload = function () {
-            oldonload();
-            ToolTip.load();
-        };
-    }
-};
-ToolTip.load = function () {
-    ToolTip.instance = new ToolTip();
-};
-ToolTip.prototype = {
-    find_trigger: function () {
-        if (!this.trigger) {
-            var el;
-            if (this.evt) {
-                el = (this.evt.target) ? this.evt.target : ((this.evt.srcElement) ? this.evt.srcElement : null);
-            }
-            var classes;
-            try {
-              (el.nodeType === 1);
-            }
-            catch (e) {
-              return false;
-            }
-            find_trigger:
-            while (el && el !== document.body && !this.trigger) {
-                if (!ToolTip.className && el.title && el.title !== '') {
-                    this.trigger = el;
-                    break;
-                } else {
-                    classes = (el.className) ? el.className.split(' ') : [];
-                    for (var i = 0; i < classes.length; i++) {
-                        if (classes[i] === ToolTip.className && el.title) {
+        },
+        show: function (evt) {
+            var evnt = (evt) ? evt : ((event) ? event : null);
+            if (evnt) {
+                var el = (evnt.target) ? evnt.target : ((evnt.srcElement) ? evnt.srcElement : null);
+                if (!this.trigger) {
+                    var classes;
+                    try {
+                        (el.nodeType === 1);
+                    }
+                    catch (e) {
+                        return false;
+                    }
+                    find_trigger:
+                    while (el && el !== document.body && !this.trigger) {
+                        if (!this.className && el.title && el.title !== '') {
                             this.trigger = el;
-                            break find_trigger;
+                            break;
+                        } else {
+                            classes = (el.className) ? el.className.split(' ') : [];
+                            for (var i = 0; i < classes.length; i++) {
+                                if (classes[i] === this.className && el.title) {
+                                    this.trigger = el;
+                                    break find_trigger;
+                                }
+                            }
                         }
+                        var mom = el.parentNode;
+                        while (mom.nodeType && mom.nodeType !== 1) {
+                            mom = mom.parentNode;
+                        }
+                        el = mom;
                     }
                 }
-                var mom = el.parentNode;
-                while (mom.nodeType && mom.nodeType !== 1) {
-                  mom = mom.parentNode;
-                }
-                el = mom;
+            }
+            if (this.trigger) {
+                this.box.style.top = (document.documentElement.scrollTop + evnt.clientY + 10) + 'px'; 
+                this.box.style.left = (document.documentElement.scrollLeft + evnt.clientX + 10) + 'px'; 
+                this.box.innerHTML = this.trigger.title;
+                this.title = this.trigger.title;
+                this.trigger.title = '';
+                this.box.style.display = 'block';
             }
         }
-        return this.trigger;
-    },
-    hide: function () {
-        if (this.find_trigger()) {
-            this.box.style.display = 'none';
-            this.find_trigger().title = this.title;
-            if (this.hidden_selects) {
-                for (var i = 0; i < this.hidden_selects.length; i++) {
-                    this.hidden_selects[i].style.visibility = 'visible';
-                }
-            }
-            this.trigger = null;
-        }
-    },
-    show: function () {
-        if (!this.trigger && this.find_trigger()) {
-            this.box.style.top = (document.documentElement.scrollTop + this.evt.clientY + 10).toString() + 'px'; 
-            this.box.style.left = (document.documentElement.scrollLeft + this.evt.clientX + 10).toString() + 'px'; 
-            this.box.innerHTML = this.find_trigger().title;
-            this.title = this.find_trigger().title;
-            this.find_trigger().title = '';
-            this.box.style.display = 'block';
-        }
-    }
-};  
-ToolTip.show = function (evt) {
-    if (!ToolTip.instance) {
-        ToolTip.instance = new ToolTip();
-    }
-    ToolTip.instance.evt = (evt) ? evt : ((event) ? event : null);
-    if (ToolTip.instance.evt) {
-        ToolTip.instance.show();
     }
 };
-
-ToolTip.init();
+Simple.add_event(window, 'load', function () {
+    Simple.Tooltip.setup();
+});
